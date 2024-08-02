4b0e5461a20af14959f0cc6287af36043b6ff4a0
Jay-Shu <jcbshuster@yahoo.com>
2024-08-01 19:42:09 -0500

    PACA-003 | getAccounts_v2

--- paca-select-statements.sql
+++ paca-select-statements.sql
@@ -10,6 +10,7 @@
     Changelog:
         2024-07-31: Created getAccounts_v1 for retrieving accounts in general.
         2024-07-31: Migrated to proper location for Git.
+        2024-08-01: Created getAccounts_v2 for adding the Where clause for Searching.
 		
 
     TO DO (Requested):
@@ -33,7 +34,14 @@
 		EXECUTING PROPERLY.
 
     Scalar Variables:
-        variableName; description
+        @intent; Enumerations as follows:
+          1 = Basic
+            no "like" used only "=",
+          2 = Search for the first name with ends-with.
+          3 = Search for the first name with starts-with.
+          4 = Search for the last name ends-with.
+          5 = Search for the last name starts-with.
+          Not including past this, as it should be within the logic for Python. Creating a v3 for additional flexibility.
 		
 	Citations:
 		1. CREATE PROCEDURE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/statements/create-procedure-transact-sql?view=sql-server-ver16
@@ -41,6 +49,8 @@
     3. ERROR_MESSAGE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/functions/error-message-transact-sql?view=sql-server-ver16
     4. CREATE_QUEUE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/statements/create-queue-transact-sql?view=sql-server-ver16
     5. CREATE_ENDPOINT (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/statements/create-endpoint-transact-sql?view=sql-server-ver16
+    6. CASE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/language-elements/case-transact-sql?view=sql-server-ver16
+    7. IF...ELSE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/language-elements/if-else-transact-sql?view=sql-server-ver16
 
 	Author Notes:
 		Inspired by my time at Hyland working with Phil Mosher and Brandon Rossin.
@@ -84,7 +94,183 @@ SELECT
   ,ERROR_STATE() AS ErrorState  
   ,ERROR_PROCEDURE() AS ErrorProcedure  
   ,ERROR_LINE() AS ErrorLine  
-  ,ERROR_MESSAGE() AS ErrorMessage;  
+  ,ERROR_MESSAGE() AS ErrorMessage;
 END CATCH
 
 GO
+
+CREATE PROCEDURE getAccounts_v2
+/*
+  Intended to use First Name and Last Name.
+  This is without the usage of like within the
+  WHERE Clause.
+  TO-DO: make multiple pathways.
+
+  
+*/
+
+@firstName NVARCHAR(64),
+@lastName NVARCHAR(64),
+@intent int
+AS
+SET NOCOUNT ON
+CASE
+  WHEN @intent = 1
+  THEN
+BEGIN TRY
+SELECT ACCOUNT_CITY,
+ACCOUNT_DATE_RENEWAL,
+ACCOUNT_DATE_START,
+ACCOUNT_FIRST_NAME,
+ACCOUNT_HONORIFICS,
+ACCOUNT_ID,
+ACCOUNT_LAST_NAME,
+ACCOUNT_NUM,
+ACCOUNT_PO_BOX,
+ACCOUNT_STATE,
+ACCOUNT_STREET_ADD_1,
+ACCOUNT_STREET_ADD_2,
+ACCOUNT_SUFFIX,
+ACCOUNT_TYPE,
+ACCOUNT_ZIP
+FROM paca.ACCOUNTS
+WHERE ACCOUNT_FIRST_NAME = @firstName
+AND ACCOUNT_LAST_NAME = @lastName
+END TRY
+BEGIN CATCH
+SELECT 
+  ERROR_NUMBER() AS ErrorNumber  
+  ,ERROR_SEVERITY() AS ErrorSeverity  
+  ,ERROR_STATE() AS ErrorState  
+  ,ERROR_PROCEDURE() AS ErrorProcedure  
+  ,ERROR_LINE() AS ErrorLine  
+  ,ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
+
+  WHEN @intent = 2
+  THEN
+BEGIN TRY
+SELECT ACCOUNT_CITY,
+ACCOUNT_DATE_RENEWAL,
+ACCOUNT_DATE_START,
+ACCOUNT_FIRST_NAME,
+ACCOUNT_HONORIFICS,
+ACCOUNT_ID,
+ACCOUNT_LAST_NAME,
+ACCOUNT_NUM,
+ACCOUNT_PO_BOX,
+ACCOUNT_STATE,
+ACCOUNT_STREET_ADD_1,
+ACCOUNT_STREET_ADD_2,
+ACCOUNT_SUFFIX,
+ACCOUNT_TYPE,
+ACCOUNT_ZIP
+FROM paca.ACCOUNTS
+WHERE ACCOUNT_FIRST_NAME like N'%' + @firstName
+AND ACCOUNT_LAST_NAME = @lastName
+END TRY
+BEGIN CATCH
+SELECT 
+  ERROR_NUMBER() AS ErrorNumber  
+  ,ERROR_SEVERITY() AS ErrorSeverity  
+  ,ERROR_STATE() AS ErrorState  
+  ,ERROR_PROCEDURE() AS ErrorProcedure  
+  ,ERROR_LINE() AS ErrorLine  
+  ,ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
+
+WHEN @intent = 3
+  THEN
+BEGIN TRY
+SELECT ACCOUNT_CITY,
+ACCOUNT_DATE_RENEWAL,
+ACCOUNT_DATE_START,
+ACCOUNT_FIRST_NAME,
+ACCOUNT_HONORIFICS,
+ACCOUNT_ID,
+ACCOUNT_LAST_NAME,
+ACCOUNT_NUM,
+ACCOUNT_PO_BOX,
+ACCOUNT_STATE,
+ACCOUNT_STREET_ADD_1,
+ACCOUNT_STREET_ADD_2,
+ACCOUNT_SUFFIX,
+ACCOUNT_TYPE,
+ACCOUNT_ZIP
+FROM paca.ACCOUNTS
+WHERE ACCOUNT_FIRST_NAME like @firstName + N'%'
+AND ACCOUNT_LAST_NAME = @lastName
+END TRY
+BEGIN CATCH
+SELECT 
+  ERROR_NUMBER() AS ErrorNumber  
+  ,ERROR_SEVERITY() AS ErrorSeverity  
+  ,ERROR_STATE() AS ErrorState  
+  ,ERROR_PROCEDURE() AS ErrorProcedure  
+  ,ERROR_LINE() AS ErrorLine  
+  ,ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
+
+WHEN @intent = 4
+  THEN
+BEGIN TRY
+SELECT ACCOUNT_CITY,
+ACCOUNT_DATE_RENEWAL,
+ACCOUNT_DATE_START,
+ACCOUNT_FIRST_NAME,
+ACCOUNT_HONORIFICS,
+ACCOUNT_ID,
+ACCOUNT_LAST_NAME,
+ACCOUNT_NUM,
+ACCOUNT_PO_BOX,
+ACCOUNT_STATE,
+ACCOUNT_STREET_ADD_1,
+ACCOUNT_STREET_ADD_2,
+ACCOUNT_SUFFIX,
+ACCOUNT_TYPE,
+ACCOUNT_ZIP
+FROM paca.ACCOUNTS
+WHERE ACCOUNT_FIRST_NAME = @firstName
+AND ACCOUNT_LAST_NAME like N'%' + @lastName
+END TRY
+BEGIN CATCH
+SELECT 
+  ERROR_NUMBER() AS ErrorNumber  
+  ,ERROR_SEVERITY() AS ErrorSeverity  
+  ,ERROR_STATE() AS ErrorState  
+  ,ERROR_PROCEDURE() AS ErrorProcedure  
+  ,ERROR_LINE() AS ErrorLine  
+  ,ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
+
+WHEN @intent = 5
+  THEN
+BEGIN TRY
+SELECT ACCOUNT_CITY,
+ACCOUNT_DATE_RENEWAL,
+ACCOUNT_DATE_START,
+ACCOUNT_FIRST_NAME,
+ACCOUNT_HONORIFICS,
+ACCOUNT_ID,
+ACCOUNT_LAST_NAME,
+ACCOUNT_NUM,
+ACCOUNT_PO_BOX,
+ACCOUNT_STATE,
+ACCOUNT_STREET_ADD_1,
+ACCOUNT_STREET_ADD_2,
+ACCOUNT_SUFFIX,
+ACCOUNT_TYPE,
+ACCOUNT_ZIP
+FROM paca.ACCOUNTS
+WHERE ACCOUNT_FIRST_NAME = @firstName
+AND ACCOUNT_LAST_NAME like @lastName + N'%'
+END TRY
+BEGIN CATCH
+SELECT 
+  ERROR_NUMBER() AS ErrorNumber  
+  ,ERROR_SEVERITY() AS ErrorSeverity  
+  ,ERROR_STATE() AS ErrorState  
+  ,ERROR_PROCEDURE() AS ErrorProcedure  
+  ,ERROR_LINE() AS ErrorLine  
+  ,ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
