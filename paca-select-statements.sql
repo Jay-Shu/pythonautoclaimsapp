f60d89ed227c19c7a063facf5c046e5d72b955d8
Jay-Shu <jcbshuster@yahoo.com>
2024-08-02 22:57:35 -0500

    Removed Trailing Spaces

--- paca-select-statements.sql
+++ paca-select-statements.sql
@@ -8,12 +8,9 @@
     Rate: 100.00 (Based on 2019*)
 
     Changelog:
-        2024-07-31: Created getAccounts_v1 for retrieving accounts in general.
-        2024-07-31: Migrated to proper location for Git.
-        2024-08-01: Created getAccounts_v2 for adding the Where clause for Searching.
-          i.e. Starts-with and Ends-With effectively (N'%' + @variableName and @variableName N'%').
+        2024-08-02: Staging for Certificates to be added to Stored Procedures
+          per best practices.
 		
-
     TO DO (Requested):
 		N/A - No current modification requests pending.
 	
@@ -35,14 +32,7 @@
 		EXECUTING PROPERLY.
 
     Scalar Variables:
-        @intent; Enumerations as follows:
-          1 = Basic
-            no "like" used only "=",
-          2 = Search for the first name with ends-with.
-          3 = Search for the first name with starts-with.
-          4 = Search for the last name ends-with.
-          5 = Search for the last name starts-with.
-          Not including past this, as it should be within the logic for Python. Creating a v3 for additional flexibility.
+        @name : Description
 		
 	Citations:
 		1. CREATE PROCEDURE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/statements/create-procedure-transact-sql?view=sql-server-ver16
@@ -52,6 +42,7 @@
     5. CREATE_ENDPOINT (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/statements/create-endpoint-transact-sql?view=sql-server-ver16
     6. CASE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/language-elements/case-transact-sql?view=sql-server-ver16
     7. IF...ELSE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/language-elements/if-else-transact-sql?view=sql-server-ver16
+    8. Tutorial: Signing Stored Procedures With a Certificate, https://learn.microsoft.com/en-us/sql/relational-databases/tutorial-signing-stored-procedures-with-a-certificate?view=sql-server-ver16
 
 	Author Notes:
 		Inspired by my time at Hyland working with Phil Mosher and Brandon Rossin.
@@ -65,6 +56,13 @@
 **/
 
 USE PACA
+
+/*
+  We need to create a signing certificate for our Stored Procedures.
+  Also, HTTP Endpoints are going to be phased out in the long-term
+  it is recommended AGAINST using such Endpoints.
+*/
+
 
 -- We need our basic query for grabbing accounts without the WITH Clause.
 CREATE PROCEDURE paca.getAccounts_v1
@@ -90,11 +88,11 @@ FROM paca.ACCOUNTS
 END TRY
 BEGIN CATCH
 SELECT 
-  ERROR_NUMBER() AS ErrorNumber  
-  ,ERROR_SEVERITY() AS ErrorSeverity  
-  ,ERROR_STATE() AS ErrorState  
-  ,ERROR_PROCEDURE() AS ErrorProcedure  
-  ,ERROR_LINE() AS ErrorLine  
+  ERROR_NUMBER() AS ErrorNumber
+  ,ERROR_SEVERITY() AS ErrorSeverity
+  ,ERROR_STATE() AS ErrorState
+  ,ERROR_PROCEDURE() AS ErrorProcedure
+  ,ERROR_LINE() AS ErrorLine
   ,ERROR_MESSAGE() AS ErrorMessage;
 END CATCH
 
@@ -137,11 +135,11 @@ AND ACCOUNT_LAST_NAME = @lastName
 END TRY
 BEGIN CATCH
 SELECT 
-  ERROR_NUMBER() AS ErrorNumber  
-  ,ERROR_SEVERITY() AS ErrorSeverity  
-  ,ERROR_STATE() AS ErrorState  
-  ,ERROR_PROCEDURE() AS ErrorProcedure  
-  ,ERROR_LINE() AS ErrorLine  
+  ERROR_NUMBER() AS ErrorNumber
+  ,ERROR_SEVERITY() AS ErrorSeverity
+  ,ERROR_STATE() AS ErrorState
+  ,ERROR_PROCEDURE() AS ErrorProcedure
+  ,ERROR_LINE() AS ErrorLine
   ,ERROR_MESSAGE() AS ErrorMessage;
 END CATCH
 
@@ -169,11 +167,11 @@ AND ACCOUNT_LAST_NAME = @lastName
 END TRY
 BEGIN CATCH
 SELECT 
-  ERROR_NUMBER() AS ErrorNumber  
-  ,ERROR_SEVERITY() AS ErrorSeverity  
-  ,ERROR_STATE() AS ErrorState  
-  ,ERROR_PROCEDURE() AS ErrorProcedure  
-  ,ERROR_LINE() AS ErrorLine  
+  ERROR_NUMBER() AS ErrorNumber
+  ,ERROR_SEVERITY() AS ErrorSeverity
+  ,ERROR_STATE() AS ErrorState
+  ,ERROR_PROCEDURE() AS ErrorProcedure
+  ,ERROR_LINE() AS ErrorLine
   ,ERROR_MESSAGE() AS ErrorMessage;
 END CATCH
 
@@ -201,11 +199,11 @@ AND ACCOUNT_LAST_NAME = @lastName
 END TRY
 BEGIN CATCH
 SELECT 
-  ERROR_NUMBER() AS ErrorNumber  
-  ,ERROR_SEVERITY() AS ErrorSeverity  
-  ,ERROR_STATE() AS ErrorState  
-  ,ERROR_PROCEDURE() AS ErrorProcedure  
-  ,ERROR_LINE() AS ErrorLine  
+  ERROR_NUMBER() AS ErrorNumber
+  ,ERROR_SEVERITY() AS ErrorSeverity
+  ,ERROR_STATE() AS ErrorState
+  ,ERROR_PROCEDURE() AS ErrorProcedure
+  ,ERROR_LINE() AS ErrorLine
   ,ERROR_MESSAGE() AS ErrorMessage;
 END CATCH
 
@@ -233,11 +231,11 @@ AND ACCOUNT_LAST_NAME like N'%' + @lastName
 END TRY
 BEGIN CATCH
 SELECT 
-  ERROR_NUMBER() AS ErrorNumber  
-  ,ERROR_SEVERITY() AS ErrorSeverity  
-  ,ERROR_STATE() AS ErrorState  
-  ,ERROR_PROCEDURE() AS ErrorProcedure  
-  ,ERROR_LINE() AS ErrorLine  
+  ERROR_NUMBER() AS ErrorNumber
+  ,ERROR_SEVERITY() AS ErrorSeverity
+  ,ERROR_STATE() AS ErrorState
+  ,ERROR_PROCEDURE() AS ErrorProcedure
+  ,ERROR_LINE() AS ErrorLine
   ,ERROR_MESSAGE() AS ErrorMessage;
 END CATCH
 
@@ -265,11 +263,11 @@ AND ACCOUNT_LAST_NAME like @lastName + N'%'
 END TRY
 BEGIN CATCH
 SELECT 
-  ERROR_NUMBER() AS ErrorNumber  
-  ,ERROR_SEVERITY() AS ErrorSeverity  
-  ,ERROR_STATE() AS ErrorState  
-  ,ERROR_PROCEDURE() AS ErrorProcedure  
-  ,ERROR_LINE() AS ErrorLine  
+  ERROR_NUMBER() AS ErrorNumber
+  ,ERROR_SEVERITY() AS ErrorSeverity
+  ,ERROR_STATE() AS ErrorState
+  ,ERROR_PROCEDURE() AS ErrorProcedure
+  ,ERROR_LINE() AS ErrorLine
   ,ERROR_MESSAGE() AS ErrorMessage;
 END CATCH
 GO
@@ -298,11 +296,11 @@ WHERE ACCOUNT_NUM = @accountNum
 END TRY
 BEGIN CATCH
 SELECT 
-  ERROR_NUMBER() AS ErrorNumber  
-  ,ERROR_SEVERITY() AS ErrorSeverity  
-  ,ERROR_STATE() AS ErrorState  
-  ,ERROR_PROCEDURE() AS ErrorProcedure  
-  ,ERROR_LINE() AS ErrorLine  
+  ERROR_NUMBER() AS ErrorNumber
+  ,ERROR_SEVERITY() AS ErrorSeverity
+  ,ERROR_STATE() AS ErrorState
+  ,ERROR_PROCEDURE() AS ErrorProcedure
+  ,ERROR_LINE() AS ErrorLine
   ,ERROR_MESSAGE() AS ErrorMessage;
 END CATCH
 GO
@@ -342,11 +340,11 @@ WHERE ACCOUNT_NUM = @accountNum
 END TRY
 BEGIN CATCH
 SELECT 
-  ERROR_NUMBER() AS ErrorNumber  
-  ,ERROR_SEVERITY() AS ErrorSeverity  
-  ,ERROR_STATE() AS ErrorState  
-  ,ERROR_PROCEDURE() AS ErrorProcedure  
-  ,ERROR_LINE() AS ErrorLine  
+  ERROR_NUMBER() AS ErrorNumber
+  ,ERROR_SEVERITY() AS ErrorSeverity
+  ,ERROR_STATE() AS ErrorState
+  ,ERROR_PROCEDURE() AS ErrorProcedure
+  ,ERROR_LINE() AS ErrorLine
   ,ERROR_MESSAGE() AS ErrorMessage;
 END CATCH
 GO
