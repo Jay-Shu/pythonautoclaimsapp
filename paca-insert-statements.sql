adba7eec79b31231de334ec3bb3a343dfde5a45c
Jay-Shu <jcbshuster@yahoo.com>
2024-08-04 10:07:50 -0500

    IU Files Addition

---
+++ paca-insert-statements.sql
@@ -0,0 +1,57 @@
+/**
+	Name of Script: paca-insert-statements.sql
+    Author: Jacob Shuster
+    Role: Consultant - 1099
+    Umbrella Company: N/A
+    Creation Date: 2024-08-04
+    Script Cost: N/A
+    Rate: 100.00 (Based on 2019*)
+
+    Changelog:
+        2024-08-04: 
+		
+    TO DO (Requested):
+		N/A - No current modification requests pending.
+	
+	TO DO (SELF):
+		Policies Enumerations. - DONE
+		Bundle Enumeration for Car and Home. For non-goal. - DONE
+		
+    DISCLAIMER:
+        After receipt, this Script is "as-is". Additional modifications past the base are at 100.00 per hour.
+        This script is intended to setup your initial Python Auto Claims App.
+    
+    Non-Billable Items:
+        Notes.
+        
+    Accessing the below variables is @nameOfVariable. The variable will ONLY be accessible within the bounds of the GO
+		Paramater. This is because each batch is committed before the next can complete.
+
+    WARNING: DO NOT MODIFY SCRIPT WITHOUT CONSULTING THE CREATOR FIRST. AS SOME CHANGES MAY PREVENT THE SCRIPT FROM
+		EXECUTING PROPERLY.
+
+    Scalar Variables:
+        @name : Description
+		
+	Citations:
+		1. CREATE PROCEDURE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/statements/create-procedure-transact-sql?view=sql-server-ver16
+    2. NO COUNT (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/statements/set-nocount-transact-sql?view=sql-server-ver16
+    3. ERROR_MESSAGE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/functions/error-message-transact-sql?view=sql-server-ver16
+    4. CREATE_QUEUE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/statements/create-queue-transact-sql?view=sql-server-ver16
+    5. CREATE_ENDPOINT (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/statements/create-endpoint-transact-sql?view=sql-server-ver16
+    6. CASE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/language-elements/case-transact-sql?view=sql-server-ver16
+    7. IF...ELSE (Transact-SQL), https://learn.microsoft.com/en-us/sql/t-sql/language-elements/if-else-transact-sql?view=sql-server-ver16
+    8. Tutorial: Signing Stored Procedures With a Certificate, https://learn.microsoft.com/en-us/sql/relational-databases/tutorial-signing-stored-procedures-with-a-certificate?view=sql-server-ver16
+
+	Author Notes:
+		Inspired by my time at Hyland working with Phil Mosher and Brandon Rossin.
+		GO Keyword does not require a semi-colon. As it would be redundant.
+		BEGIN...END Blocks have been phased out completely for TRY...CATCH and GO.
+        BEGIN TRANSACTION...COMMIT TRANSACTION Blocks to be used. As it is intended
+            for the application to be running these statements.
+    These will need to be transitioned into Stored Procedures. This is necessary for
+      exposing an external Web service.
+
+**/
+
+USE PACA
