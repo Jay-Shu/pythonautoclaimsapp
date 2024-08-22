2968435062040e19da30210926c9abc97a7901fa
Jay-Shu <jcbshuster@yahoo.com>
2024-08-21 20:18:42 -0500

    Read File

---
+++ src/pythonautoclaimsapp/customscript/connect_to_mssql_server.py
@@ -0,0 +1,35 @@
+"""
+    Name of Script: connect_to_mssql_server.py
+    Author: Jacob Shuster
+    Role: Consultant - 1099
+    Umbrella Company: N/A
+    Creation Date: 2024-08-11
+    Script Cost: N/A
+    Rate: 100.00 (Based on 2019*)
+
+    Changelog:
+        2024-08-11: Updated naming convention of script. Hyphens cannot be used.
+            It must be underscores.
+    
+    Functions
+
+    
+    Author Notes:
+        Our connection to MS SQL Server.
+"""
+
+import pyodbc
+
+# Specifying the ODBC driver, server name, database, etc. directly
+
+def connect():
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=127.0.0.1;DATABASE=PACA;UID=pacauser;PWD=pacauser;TrustServerCertificate=YES;Encrypt=YES;SaveFile=connecttest.txt')
+    cnxn.setencoding('utf-8')
+    return cnxn
+
+connect()
+
+
+
+"""def getAccounts(accountNum): """
+    
